b08496febd750ff00ff5f3478b626e6787519431
Forrest Williams <ffwilliams2@alaska.edu>
2024-09-18 20:22:48 +0000

    fixes befor re-run

--- src/opera_disp_tms/generate_coh_tiles.py
+++ src/opera_disp_tms/generate_coh_tiles.py
@@ -1,5 +1,3 @@
-import json
-import os
 from itertools import product
 from pathlib import Path
 
@@ -7,7 +5,6 @@ import boto3
 from botocore import UNSIGNED
 from botocore.client import Config
 from botocore.exceptions import ClientError
-from mapbox_tilesets.scripts.cli import upload_raster_source  # create, publish
 from osgeo import gdal
 
 
@@ -17,8 +14,6 @@ gdal.UseExceptions()
 S3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
 SOURCE_S3 = 's3://sentinel-1-global-coherence-earthbigdata/data/tiles'
 UPLOAD_S3 = 's3://opera-disp-tms-dev'
-# MAPBOX_ACCOUNT = os.environ['MAPBOX_ACCOUNT']
-# MAPBOX_ACCESS_TOKEN = os.environ['MAPBOX_ACCOUNT']
 
 
 def create_coh_s3_path(lon, lat, coh_prod='summer_vv_COH12'):
@@ -44,7 +39,7 @@ def download_coh(s3_path):
         return s3_path
 
 
-def create_coh_tile(min_lon, min_lat, max_lon, max_lat, coh_prod='summer_vv_COH12', suffix=None):
+def create_coh_tile(min_lon, min_lat, max_lon, max_lat, coh_prod='summer_vv_COH12', gtiff=True):
     s3_paths = []
     for lon in range(min_lon, max_lon):
         for lat in range(min_lat, max_lat):
@@ -55,7 +50,13 @@ def create_coh_tile(min_lon, min_lat, max_lon, max_lat, coh_prod='summer_vv_COH1
     s3_paths = [download_coh(x) for x in s3_paths]
     names = [x.split('/')[-1] for x in s3_paths if x is not None]
 
-    product_name = coh_prod if suffix is None else f'{coh_prod}_{suffix}'
+    min_lat_str = f'N{abs(min_lat):02d}' if min_lat >= 0 else f'S{abs(min_lat):02d}'
+    min_lon_str = f'E{abs(min_lon):03d}' if min_lon >= 0 else f'W{abs(min_lon):03d}'
+    max_lat_str = f'N{abs(max_lat):02d}' if max_lat >= 0 else f'S{abs(max_lat):02d}'
+    max_lon_str = f'E{abs(max_lon):03d}' if max_lon >= 0 else f'W{abs(max_lon):03d}'
+    bbox_str = f'{min_lat_str}{min_lon_str}_{max_lat_str}{max_lon_str}'
+
+    product_name = f'{coh_prod}_{bbox_str}'
     vrt_path = f'{product_name}.vrt'
     output_path = f'{product_name}.tif'
 
@@ -65,12 +66,11 @@ def create_coh_tile(min_lon, min_lat, max_lon, max_lat, coh_prod='summer_vv_COH1
         vrt.FlushCache()
         vrt = None
 
-        # creation_opts = ['TILED=YES', 'COMPRESS=LZW', 'NUM_THREADS=ALL_CPUS']
-        # gdal.Translate(output_path, vrt_path, format='GTiff', creationOptions=creation_opts)
-        # gdal.Translate(output_path, vrt_path, format='COG', creationOptions=['COMPRESS=LZW'])
-        # gdal.Translate(f'{product_name}.mbtiles', vrt_path, format='MBTILES')
-        # warp_options = gdal.WarpOptions(format='GTiff', dstSRS='EPSG:3857', xRes=30, yRes=30)
-        warp_options = gdal.WarpOptions(format='COG', dstSRS='EPSG:3857', xRes=30, yRes=30)
+        if gtiff:
+            opts = ['TILED=YES', 'COMPRESS=LZW', 'NUM_THREADS=ALL_CPUS']
+            warp_options = gdal.WarpOptions(format='GTiff', dstSRS='EPSG:3857', xRes=30, yRes=30, creationOptions=opts)
+        else:
+            warp_options = gdal.WarpOptions(format='COG', dstSRS='EPSG:3857', xRes=30, yRes=30)
         gdal.Warp(output_path, vrt_path, options=warp_options)
 
         Path(vrt_path).unlink()
@@ -98,8 +98,8 @@ def create_coh_tile_set(min_lon, min_lat, max_lon, max_lat, n_parts_lon=1, n_par
     lat_ranges = split_range(min_lat, max_lat, n_parts_lat)
     lon_lat_bboxes = [(lon[0], lat[0], lon[1], lat[1]) for lon, lat in product(lon_ranges, lat_ranges)]
     output_paths = []
-    for i, lon_lat_box in enumerate(lon_lat_bboxes):
-        output_path = create_coh_tile(*lon_lat_box, coh_prod=coh_prod, suffix=i + 1)
+    for lon_lat_box in lon_lat_bboxes:
+        output_path = create_coh_tile(*lon_lat_box, coh_prod=coh_prod)
         output_paths.append(output_path)
     print('All mosaics complete')
 
@@ -111,51 +111,13 @@ def upload_tileset_s3(prefix='summer_vv_COH12'):
         s3.upload_file(str(file), UPLOAD_S3.split('/')[-1], f'{prefix}/{file.name}')
 
 
-def create_template(
-    mapbox_account, tileset_name, minzoom=2, maxzoom=6, offset=0, scale=1, units='meters', outname='template.json'
-):
-    template = {
-        'version': 1,
-        'type': 'rasterarray',
-        'sources': [{'uri': f'mapbox://tileset-source/{mapbox_account}/{tileset_name}'}],
-        'minzoom': minzoom,
-        'maxzoom': maxzoom,
-        'layers': {
-            '12-Day Summber Coherence': {
-                'tilesize': 512,
-                'offset': offset,
-                'scale': scale,
-                'resampling': 'bilinear',
-                'buffer': 1,
-                'units': units,
-            },
-        },
-    }
-    with open(outname, 'w') as f:
-        f.write(json.dumps(template, indent=2))
-
-
-def upload_tileset(username, id, wildcard='summer_vv_COH12'):
-    files = [str(x) for x in Path.cwd().glob(f'{wildcard}*.tif')][:0]
-    ctx = dict()
-    upload_raster_source(ctx, username, id, files)
-
-
-### Tileset CLI commands:
-# tilesets upload-raster-source ffwilliams2 summer-vv-coh12-source ./summer_vv_COH12_1.tif
-# tilesets create ffwilliams2.summer-vv-coh12 --recipe ./summer-vv-coh12.json --name "Coherence 12-Day Summer"
-# tilesets publish ffwilliams2.summer-vv-coh12
-
-
 if __name__ == '__main__':
-    mapbox_source_name = 'summer-vv-coh12'
-    recipe_location = './summer-vv-coh12.json'
-    source_name = 'summer-vv-coh12-source'
-
     # Full North America: 6,148 tiles, 34 GB uncompressed, 24 compressed
-    lon_lat_box = [-169, 14, -63, 72]
-    # lon_lat_box = [-119, 36, -115, 40]
-    output_paths = create_coh_tile_set(*lon_lat_box, n_parts_lon=5, n_parts_lat=5)
-    upload_tileset_s3()
+    # lon_lat_box = [-169, 14, -63, 72]
+    # output_paths = create_coh_tile_set(*lon_lat_box, n_parts_lon=5, n_parts_lat=5)
 
-    # create_template(mapbox_account=MAPBOX_ACCOUNT, tileset_name=source_name, outname=recipe_location, maxzoom=12)
+    # Smaller test_area
+    lon_lat_box = [-119, 36, -115, 40]
+    output_paths = create_coh_tile_set(*lon_lat_box, n_parts_lon=2, n_parts_lat=2)
+
+    # upload_tileset_s3()
