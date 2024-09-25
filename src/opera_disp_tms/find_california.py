d3549c08148d45a05511506cc48d7fe46ae4885c
Forrest Williams <ffwilliams2@alaska.edu>
2024-09-25 10:47:45 -0500

    update script to identify california data

--- src/opera_disp_tms/find_california.py
+++ src/opera_disp_tms/find_california.py
@@ -1,12 +1,20 @@
-"""Find all OPERA L3 DISP S1 PROVISIONAL V0.4 granules created for California test dataset."""
+"""Find all OPERA L3 DISP S1 PROVISIONAL V0.4 granules created for California test dataset.
+see https://github.com/nasa/opera-sds/issues/54 for dataset creation request.
+"""
 from datetime import datetime
+from typing import List
 
 import asf_search as asf
 
 
-# https://github.com/nasa/opera-sds/issues/54
-issue_open_date = datetime.strptime('20240917T144641Z', '%Y%m%dT%H%M%SZ')
-des_frames = [
+DATE_FORMAT = '%Y%m%dT%H%M%SZ'
+
+PROCESSING_START = datetime.strptime('20240917T173251Z', DATE_FORMAT)
+PROCESSING_END = datetime.strptime('20240922T154629Z', DATE_FORMAT)
+DATA_START = datetime.strptime('20160701T000000Z', DATE_FORMAT)
+DATA_END = datetime.strptime('20180702T000000Z', DATE_FORMAT)
+
+DES_FRAMES = [
     3325,
     3326,
     3327,
@@ -40,7 +48,7 @@ des_frames = [
     46290,
     46291,
 ]
-asc_frames = [
+ASC_FRAMES = [
     9154,
     9155,
     9156,
@@ -58,7 +66,7 @@ asc_frames = [
     16946,
     24726,
     24727,
-    # 24728, # Not enough scenes avaialable
+    24728,  # Data is not available for this frame
     28757,
     28758,
     28759,
@@ -75,46 +83,64 @@ asc_frames = [
     44328,
     44329,
 ]
-cal_frams = sorted(asc_frames + des_frames)
+CAL_FRAMES = sorted(ASC_FRAMES + DES_FRAMES)
 
-session = asf.ASFSession(edl_host='uat.urs.earthdata.nasa.gov')
-disp_opts = asf.ASFSearchOptions(
-    session=session, host='cmr.uat.earthdata.nasa.gov', shortName='OPERA_L3_DISP-S1_PROVISIONAL_V0'
-)
-disp_opts.maxResults = None
-cmr_keywords = [('options[readable_granule_name][pattern]', 'true')]
-# granule_name_wildcard = ['*_v0.4_*']
-granule_name_wildcard = ['*']
-results = asf.search(opts=disp_opts, cmr_keywords=cmr_keywords, granule_list=granule_name_wildcard)
 
-frames = []
-creation_dates = []
-desired_results = []
-versions = []
-for result in results:
-    scene_name = result.properties['sceneName']
-    frame = int(scene_name.split('_')[4][1:])
-    creation_date = datetime.strptime(scene_name.split('_')[-1], '%Y%m%dT%H%M%SZ')
-    version = scene_name.split('_')[-2]
-    if creation_date >= issue_open_date:
-        desired_results.append(result)
-        frames.append(frame)
-        creation_dates.append(creation_date)
-        versions.append(version)
-frames = sorted(list(set(frames)))
-versions = sorted(list(set(versions)))
+def find_test_data(desired_version: str = '0.4') -> asf.ASFSearchResults:
+    """Find all OPERA L3 DISP S1 PROVISIONAL granules created for a specific version.
 
-breakpoint()
-print(f'Found {len(desired_results)} results')
-print('Earliest creation date:', min(creation_dates))
-print('Latest creation date:', max(creation_dates))
+    Args:
+        desired_version: The version of the granules to search for.
 
-missing_frames = list(set(cal_frams) - set(frames))
-if len(missing_frames) > 0:
-    print('Missing frames:', missing_frames)
-else:
-    print('All frames accounted for')
+    Returns:
+        A list of granules that match the search criteria.
+    """
+    session = asf.ASFSession(edl_host='uat.urs.earthdata.nasa.gov')
+    disp_opts = asf.ASFSearchOptions(
+        session=session, host='cmr.uat.earthdata.nasa.gov', shortName='OPERA_L3_DISP-S1_PROVISIONAL_V0'
+    )
+    disp_opts.maxResults = None
+    cmr_keywords = [('options[readable_granule_name][pattern]', 'true')]
+    granule_name_wildcard = [f'*_v{desired_version}_*']
+    granule_name_wildcard = ['*']
+    results = asf.search(opts=disp_opts, cmr_keywords=cmr_keywords, granule_list=granule_name_wildcard)
+    return results
 
-extra_frames = list(set(frames) - set(cal_frams))
-if len(extra_frames) > 0:
-    print('Extra frames:', extra_frames)
+
+def filter_restults_to_california_dataset(results: asf.ASFSearchResults) -> List[asf.ASFProduct]:
+    """Filter the search results to only include granules from the California test dataset.
+
+    Args:
+        results: The search results to filter.
+
+    Returns:
+        A list of granules that are part of the California test dataset.
+    """
+    desired_results = []
+    for result in results:
+        scene_name = result.properties['sceneName']
+        frame = int(scene_name.split('_')[4][1:])
+        reference_date = datetime.strptime(scene_name.split('_')[-4], DATE_FORMAT)
+        secondary_date = datetime.strptime(scene_name.split('_')[-3], DATE_FORMAT)
+        creation_date = datetime.strptime(scene_name.split('_')[-1], DATE_FORMAT)
+
+        reference_within_data_date_range = DATA_START <= reference_date <= DATA_END
+        secondary_within_data_date_range = DATA_START <= secondary_date <= DATA_END
+        within_data_date_range = reference_within_data_date_range and secondary_within_data_date_range
+        within_processing_date_range = PROCESSING_START <= creation_date <= PROCESSING_END
+        desired_frame = frame in CAL_FRAMES
+
+        if within_data_date_range and within_processing_date_range and desired_frame:
+            desired_results.append(result)
+    return desired_results
+
+
+def find_california_dataset() -> List[asf.ASFProduct]:
+    """Find all OPERA L3 DISP S1 PROVISIONAL V0.4 granules created for California test dataset.
+
+    Returns:
+        A list of granules that are part of the California test dataset.
+    """
+    results = find_test_data()
+    desired_results = filter_restults_to_california_dataset(results)
+    return desired_results
