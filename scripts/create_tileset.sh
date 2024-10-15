282d994b739c24797b22c2608948bde839ddfbce
Forrest Williams <ffwilliams2@alaska.edu>
2024-10-15 08:17:56 -0500

    add openlayers link

--- scripts/create_tileset.sh
+++ scripts/create_tileset.sh
@@ -2,3 +2,5 @@ set -x
 
 ls *.tif | xargs create_tile_map tiles/
 # aws s3 cp tiles/ s3://BURCKET/YOUR/PREFIX --recursive
+# View result at:
+# https://BUCKET.s3.amazonaws.com/YOUR/BUCKET/openlayers.html
