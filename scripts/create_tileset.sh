3029345af37490cd565398a074e3119206849e6b
Forrest Williams <ffwilliams2@alaska.edu>
2024-10-16 10:23:46 -0500

    remove bash scripts

--- scripts/create_tileset.sh
+++
@@ -1,6 +0,0 @@
-set -x
-
-ls *.tif | xargs create_tile_map tiles/
-# aws s3 cp tiles/ s3://BURCKET/YOUR/PREFIX --recursive
-# View result at:
-# https://BUCKET.s3.amazonaws.com/YOUR/BUCKET/openlayers.html
