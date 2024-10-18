53eeeecf843f974fb2ca94c2950fd1458a5adba7
Forrest Williams <ffwilliams2@alaska.edu>
2024-10-18 21:28:25 +0000

    more numba work

--- src/opera_disp_tms/generate_sw_vel_tile.py
+++ src/opera_disp_tms/generate_sw_vel_tile.py
@@ -2,9 +2,9 @@ import argparse
 from datetime import datetime
 from pathlib import Path
 
-import numba
 import numpy as np
 import xarray as xr
+from numba import float32, njit, prange
 from osgeo import gdal
 from rasterio.transform import Affine
 
@@ -21,82 +21,11 @@ def get_years_since_start(datetimes):
     return yrs_since_start
 
 
-# def linear_regression_np(data, x):
-#     n, m, p = data.shape
-#     slope = np.zeros((m, p))
-#     intercept = np.zeros((m, p))
-#
-#     X = np.vstack([x, np.ones_like(x)]).T
-#
-#     for i in range(m):
-#         for j in range(p):
-#             y = data[:, i, j]
-#             slope[i, j], intercept[i, j] = np.linalg.lstsq(X, y, rcond=None)[0]
-#
-#     return slope, intercept
-#
-#
-# @numba.njit
-# def linear_regression_leastsquares_bad(X, y):
-#     n = len(X)
-#     X_mean = np.mean(X)
-#     y_mean = np.mean(y)
-#
-#     numerator = 0.0
-#     denominator = 0.0
-#
-#     for i in range(n):
-#         numerator += (X[i] - X_mean) * (y[i] - y_mean)
-#         denominator += (X[i] - X_mean) ** 2
-#
-#     if denominator == 0:
-#         return np.nan, np.nan
-#
-#     slope = numerator / denominator
-#     intercept = y_mean - slope * X_mean
-#     return slope, intercept
-
-
-# @numba.njit
-# def linear_regression_leastsquares(X, y):
-#     if np.all(np.isnan(X)):
-#         return np.nan, np.nan
-#
-#     if np.all(np.isclose(y, y[0], atol=1e-6)):
-#         return 0.0, y[0]
-#
-#     X_b = np.zeros((X.shape[0], 2), dtype=X.dtype)
-#     X_b[:, 0] = X
-#     X_b[:, 1] = np.ones_like(X)
-#
-#     step1 = np.dot(X_b.T, X_b)
-#     step2 = np.linalg.inv(step1)
-#     step3 = np.dot(step2, X_b.T)
-#     theta = np.dot(step3, y)
-#     slope, intercept = theta[0], theta[1]
-#     return slope, intercept
-
-
-# @numba.njit
-# def linear_regression_leastsquares(X, y):
-#     if np.all(np.isnan(X)):
-#         return np.nan, np.nan
-#
-#     if np.all(np.isclose(y, y[0], atol=1e-6)):
-#         return 0.0, y[0]
-#
-#     X_b = np.zeros((X.shape[0], 2), dtype=X.dtype)
-#     X_b[:, 0] = X
-#     X_b[:, 1] = np.ones_like(X)
-#     slope, intercept = np.linalg.lstsq(X_b, y, rcond=None)[0]
-#     return slope, intercept
-
-
-@numba.njit
+@njit#(float32[::1](float32[::1], float32[::1]), cache=True)
 def linear_regression_leastsquares(x, y):
     """From: https://www4.stat.ncsu.edu/%7Edickey/courses/st512/pdf_notes/Formulas.pdf"""
-    if np.all(np.isnan(x)):
-        return np.nan, np.nan
+    if np.all(np.isnan(x)) or np.all(np.isclose(x, x[0], atol=1e-6)):
+        return np.nan
 
     n = len(x)
     x_mean = np.mean(x)
@@ -105,22 +34,20 @@ def linear_regression_leastsquares(x, y):
     sum_cross = np.sum(x * y)
     x_correction = x_square_sum - (n * x_mean**2)
     if np.isclose(x_correction, 0.0, atol=1e-6):
-        return np.nan, np.nan
+        return np.nan
     xy_correction = sum_cross - (n * x_mean * y_mean)
     slope = xy_correction / x_correction
-    intercept = y_mean - (slope * x_mean)
-    return slope, intercept
+    return slope
 
 
-@numba.njit(parallel=True)
+@njit(parallel=True)
 def parallel_linear_regression(X, y):
     n, m, p = X.shape
     slope = np.zeros((m, p))
-    intercept = np.zeros((m, p))
-    for i in numba.prange(m):
-        for j in numba.prange(p):
-            slope[i, j], intercept[i, j] = linear_regression_leastsquares(X[:, i, j].copy(), y)
-    return slope, intercept
+    for i in prange(m):
+        for j in prange(p):
+            slope[i, j] = linear_regression_leastsquares(X[:, i, j].copy(), y)
+    return slope
 
 
 def add_velocity_data_to_array(granules, frame, geotransform, frame_map_array, out_array):
@@ -134,7 +61,7 @@ def add_velocity_data_to_array(granules, frame, geotransform, frame_map_array, o
     cube = cube.where(non_nan_count >= 2, np.nan)
 
     cube_array = cube.data * 1_000_000  # convert to um
-    slope, intercept = parallel_linear_regression(cube_array, cube.years_since_start.data.astype('float64'))
+    slope, intercept = parallel_linear_regression(cube_array, cube.years_since_start.data.astype('float32'))
     new_coords = {'x': cube.x, 'y': cube.y, 'spatial_ref': cube.spatial_ref}
     slope_da = xr.DataArray(slope, dims=('y', 'x'), coords=new_coords)
     velocity = xr.Dataset({'velocity': slope_da * 10_000, 'count': non_nan_count}, new_coords)
--- tests/test_generate_sw_vel_tile.py
+++ tests/test_generate_sw_vel_tile.py
@@ -6,16 +6,15 @@ from opera_disp_tms import generate_sw_vel_tile as sw_vel
 def test_linear_regression_leastsquares():
     x = np.arange(10, dtype='float32')
     y = np.arange(10, dtype='float32')
-    slope, intercept = sw_vel.linear_regression_leastsquares(x, y)
+    slope = sw_vel.linear_regression_leastsquares(x, y)
     assert np.isclose(slope, 1.0, atol=1e-6)
-    assert np.isclose(intercept, 0.0, atol=1e-6)
 
-    slope, intercept = sw_vel.linear_regression_leastsquares(x * -1, y + 2)
+    slope = sw_vel.linear_regression_leastsquares(x * -1, y + 2)
     assert np.isclose(slope, -1.0, atol=1e-6)
-    assert np.isclose(intercept, 2.0, atol=1e-6)
 
     x_1 = np.ones(10, dtype='float32')
-    slope, intercept = sw_vel.linear_regression_leastsquares(x_1, y)
+    slope = sw_vel.linear_regression_leastsquares(x_1, y)
+    assert np.isnan(slope)
 
 
 def test_parallel_linear_regression():
@@ -26,6 +25,5 @@ def test_parallel_linear_regression():
         for j in indexes:
             x[:, i, j] = x_col
     y = np.arange(3, dtype='float32')
-    slope, intercept = sw_vel.parallel_linear_regression(x, y)
+    slope = sw_vel.parallel_linear_regression(x, y)
     assert np.all(np.isclose(slope, 1.0, atol=1e-6))
-    assert np.all(np.isclose(intercept, 0.0, atol=1e-6))
