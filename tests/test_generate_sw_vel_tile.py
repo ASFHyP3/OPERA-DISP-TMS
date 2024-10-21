0cc0950b2a8251fb425b68aa22cde19d91dc2cea
Forrest Williams <ffwilliams2@alaska.edu>
2024-10-21 17:04:47 -0500

    fix test

--- tests/test_generate_sw_vel_tile.py
+++ tests/test_generate_sw_vel_tile.py
@@ -45,12 +45,12 @@ def test_linear_regression_leastsquares():
 
 
 def test_parallel_linear_regression():
-    x_col = np.arange(3, dtype='float64')
-    x = np.ones((3, 5, 5), dtype='float64') * -1
+    y_col = np.arange(3, dtype='float64')
+    y = np.ones((3, 5, 5), dtype='float64') * -1
     indexes = np.arange(5)
     for i in indexes:
         for j in indexes:
-            x[:, i, j] = x_col
-    y = np.arange(3, dtype='float64')
+            y[:, i, j] = y_col
+    x = np.arange(3, dtype='float64')
     slope = sw_vel.parallel_linear_regression(x, y)
     assert np.all(np.isclose(slope, 1.0, atol=1e-6))
