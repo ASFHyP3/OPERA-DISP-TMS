564e49a65f2402a98b119be5ae6ccb1737b2b426
Forrest Williams <ffwilliams2@alaska.edu>
2024-12-03 19:52:37 +0000

    add netrc logic

--- src/opera_disp_tms/__main__.py
+++ src/opera_disp_tms/__main__.py
@@ -3,8 +3,11 @@ OPERA-DISP Tile Map Service Generator
 """
 
 import argparse
+import os
 import sys
 from importlib.metadata import entry_points
+
+from hyp3lib.fetch import write_credentials_to_netrc_file
 
 
 def main():
@@ -20,6 +23,11 @@ def main():
         help='Select the HyP3 entrypoint to use',  # HyP3 entrypoints are specified in `pyproject.toml`
     )
     args, unknowns = parser.parse_known_args()
+
+    username = os.getenv('EARTHDATA_USERNAME')
+    password = os.getenv('EARTHDATA_PASSWORD')
+    if username and password:
+        write_credentials_to_netrc_file(username, password, append=False)
 
     # NOTE: Cast to set because of: https://github.com/pypa/setuptools/issues/3649
     (process_entry_point,) = set(entry_points(group='hyp3', name=args.process))
